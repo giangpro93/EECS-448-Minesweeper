878a8bce3dd40348fe5499e6fd05f6c1d901c9e4
Michael Svoren <msvoren@ku.edu>
2019-02-06 17:06:00 -0600

    added array

--- MSw_implementation/Board.rb
+++ MSw_implementation/Board.rb
@@ -3,13 +3,16 @@ class Board
 	@@m_cols = 0
 	@@m_numMines = 0
 	@@m_numFlags = 0
+	@@m_board = []
 
 	def initialize(rows, cols, mines)
 		m_rows = rows
 		m_cols = cols
 		m_numMines = mines
 		m_numFlags = mines
+		m_board = Array.new(rows){Array.new(cols)}
 	end
+
 
 	
 end
