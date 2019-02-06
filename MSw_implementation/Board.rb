f1f885ea555eb9376d254f49254813be6fde2504
Nathan Nichols <n545n545@ku.edu>
2019-02-06 17:26:28 -0600

    added getters for cols and rows, and added for loops for printing board

--- MSw_implementation/Board.rb
+++ MSw_implementation/Board.rb
@@ -1,19 +1,34 @@
 class Board
-	@@m_rows = 0
-	@@m_cols = 0
-	@@m_numMines = 0
-	@@m_numFlags = 0
-	@@m_board = []
+	@m_rows = 0
+	@m_cols = 0
+	@m_numMines = 0
+	@m_numFlags = 0
+	@m_board = []
 
 	def initialize(rows, cols, mines)
-		m_rows = rows
-		m_cols = cols
-		m_numMines = mines
-		m_numFlags = mines
-		m_board = Array.new(rows){Array.new(cols)}
+		@m_rows = rows
+		@m_cols = cols
+		@m_numMines = mines
+		@m_numFlags = mines
+		@m_board = Array.new(rows){Array.new(cols)}
 	end
 
+	#returns number of rows
+	def getRows()
+		return @m_rows
+	end
+	#returns number of columns
+	def getCols()
+		return @m_cols
+	end
 
-	
+	#prints board to terminal(currently)
+	def printBoard()
+		for x in (0..(@m_rows-1))
+			for y in (0..(@m_cols-1))
+				
+			end
+		end	
+	end
 end
 
