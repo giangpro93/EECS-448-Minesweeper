1b89deb860323846b8890f0ae6019c9c85cba645
Nathan Nichols <n545n545@ku.edu>
2019-02-06 18:24:20 -0600

    Board now initializes array of Spaces, and will print spaces

--- MSw_implementation/Board.rb
+++ MSw_implementation/Board.rb
@@ -10,7 +10,14 @@ class Board
 		@m_cols = cols
 		@m_numMines = mines
 		@m_numFlags = mines
-		@m_board = Array.new(rows){Array.new(cols)}
+
+		@m_board = Array.new(rows) {Array.new(rows,cols)}
+	
+		for x in (0..(@m_rows-1))
+			for y in (0..(@m_cols-1))
+				@m_board[x][y] = BoardSpace.new()
+			end
+		end	
 	end
 
 	#returns number of rows
@@ -26,9 +33,11 @@ class Board
 	def printBoard()
 		for x in (0..(@m_rows-1))
 			for y in (0..(@m_cols-1))
-				
+				print @m_board[x][y].getSpace()
 			end
+			print "\n"
 		end	
 	end
 end
+
 
