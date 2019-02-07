41f2b35ab5e8870b1b5c20e4dd720ad790ee7bf9
Michael Svoren <msvoren@ku.edu>
2019-02-06 18:59:48 -0600

    update

--- MSw_implementation/Board.rb
+++ MSw_implementation/Board.rb
@@ -1,9 +1,9 @@
 class Board
-	@@m_rows = 0
-	@@m_cols = 0
-	@@m_numMines = 0
-	@@m_numFlags = 0
-	@@m_board = []
+	@m_rows = 0
+	@m_cols = 0
+	@m_numMines = 0
+	@m_numFlags = 0
+	@m_board = []
 
 	def initialize(rows, cols, mines)
 		m_rows = rows
@@ -11,8 +11,28 @@ class Board
 		m_numMines = mines
 		m_numFlags = mines
 		m_board = Array.new(rows){Array.new(cols)}
+		#initialize all spots in board to have a boardspace, may not be necessary
+		#set mines
+		for i in (0...m_rows)
+			for j in (0...m_cols)
+				m_board[i][j] = boardspace.new
+			end
+		end
 	end
 
+	#initalize all board spaces by setting mines around the users' starting spot
+	def run(startingX, startingY)
+		#set mines
+		for i in (0...m_rows)
+			for j in (0...m_cols)
+				if _____ then
+					m_board
+				
+
+			end
+		end
+
+	end
 
 	
 end
