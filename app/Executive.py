11ac925f9845dcef9f6a81eb9574e91a089e70bf
Michael Svoren <msvoren@ku.edu>
2019-02-17 15:54:10 -0600

    begin integration of webserver and executive

--- app/Executive.py
+++ app/Executive.py
@@ -5,27 +5,19 @@ import json
 class Executive:
 
     '''Define member variables'''
-    m_rows = 0
-    m_cols = 0
-    m_numMines = 0
-    gameOver = False
-    wonGame = False
     m_board = Board(5, 5, 2)
     firstMove = True
+    m_userID = 0
 
-    def __init__(self, rows, cols, numMines):
+    def __init__(self, rows, cols, numMines, userID):
         if rows < 2 or cols < 2:
             raise RuntimeError('Invalid board size')
         if numMines < 1 or numMines > rows * cols - 1:
             raise RuntimeError('Invalid number of mines')
 
-        self.m_rows = rows
-        self.m_cols = cols
-        self.m_numMines = numMines
-        self.gameOver = False
-        self.wonGame = False
-        self.m_board = Board(self.m_rows, self.m_cols, self.m_numMines)
+        self.m_board = Board(rows, cols, numMines)
         self.firstMove = True
+        self.m_userID = userID
         return
 
     def leftClick(self, row, col):
@@ -56,3 +48,6 @@ class Executive:
             return 1
         else:
             return 0
+
+    def getUserID(self):
+        return self.m_userID
--- app/webserver.py
+++ app/webserver.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, Response
+from Executive import Executive
 import os.path
 import requests
 import json
 
 app = Flask(__name__)
+
+#hold lists of user games
+games = []
+#hold current userID (increments by 1)
+userID = 80046264357
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,16 +33,28 @@ def main():
 @app.route('/api/createBoard', methods=['POST'])
 def api_newboard():
     print(request.form.to_dict()['json_string'])
+
+    #add new game to list of games
+    newGame = Executive('''rows, cols, numMines''', userID)
+    games.append(newGame)
+    #increment userID by 1
+    userID = userID + 1
+
     # POST with JSON
     return str(True)
 
 
 @app.route('/api/selectSpace', methods=['GET', 'POST'])
-def api_selectSpace():
+def api_selectSpace(self):
     print(request.form.to_dict()['json_string'])
     # POST with JSON
     payload = [[1, 2, 3], [1, 2, 3], [-1, -1, -1]]
     r = json.dumps(payload)
+
+    for i in self.games:
+        if (games[i].getUserID() == '''passed in userID'''):
+            #call either right or left click method
+
     return str(r)
 
 
