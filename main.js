32b24f3a02a67e02c138dfddef6099508934e343
TiernonRR <tiernon.riesenmy@gmail.com>
2019-02-17 13:44:15 -0600

    Code to interpret JSON board data, and post user input to url

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 $board = $('#board');
 const url='our_url';
+var cols = null;
+var rows = null;
 
 function onSubmit(){
   //get user input
-  const rows = document.getElementById("num_rows").value;
-  const cols = document.getElementById("num_cols").value;
+  rows = document.getElementById("num_rows").value;
+  cols = document.getElementById("num_cols").value;
   const mines = document.getElementById('num_mines').value;
-
-  createUniqueID();
+  const userID = createUniqueID();
+  $.post(url, {
+    json_string: JSON.stringify({rows: rows, cols: cols, mines: mines, userID: userID})
+  });
   createBoard(rows, cols);
-
   let form = document.getElementById('board-def');
   form.style.display = 'none';
-
   return false;
 }
 
 function createUniqueID(){
   //create random number for userID
   let userID = Math.floor((Math.random() * 1000000) + 1);
+  return(userID);
 }
 
 function createBoard(r, c){
@@ -68,21 +71,37 @@ $board.on('click', '.col.hidden', function(){
     //receive new row and col values to change board
     $.get(url, function(data){
 
-      if(data.gameOver){
+      if(data == "gameOver"){
         gameOver();
       }
       else{
-        for(i in data.field){
-          for(j in data.field[i]){
-            if(data.field[i][j].type == "adjacent"){
-              var numAdjacent = data.field[i][j].adjacentNum;
-              $('<div class=divText>' + numAdjacent + '</div>').appendTo($block);
-            }
-            else if(data.field[i][j].type == "openTile"){
-              $block$(this).css('background-color: ', white);
-            }
+        var countRow = 0;
+        var countCol = 0;
+
+        for(i in data){
+
+          if(i > cols){
+            countCol=0;
+            countRow++;
           }
+
+          let curSpace = $('.col.hidden[data-row=${countRow}][data-col=${countCol}]')
+
+          if(data[i] == '0'){
+            clearSpace(curSpace);
+          }
+          else if(data[i] == 'f'){
+            flagSpace(curSpace);
+          }
+          else{
+            var numAdjacent = data[i];
+            $('<div class=divText>' + numAdjacent + '</div>').appendTo(curSpace);
+          }
+
+          counCol++;
+
         }
+
       }
     })
 
